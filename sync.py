e8f222e05a509f64d91eab878653a3d20ccd7ce7
Christoph Jud <christoph.jud@protonmail.com>
2024-05-07 08:48:17 +0200

    added sync for users

--- sync.py
+++ sync.py
@@ -24,6 +24,7 @@ LAKEFS_SECRET_ACCESS_KEY = os.environ.get('LAKEFS_SECRET_ACCESS_KEY')
 LAKEFS_ENDPOINT = os.environ.get('LAKEFS_ENDPOINT')
 
 GROUP_MATCH = os.environ.get('GROUP_FILTER', '*')
+USER_MATCH = os.environ.get('USER_FILTER', '*m365*')
 LAKEFS_DEFAULT_POLICIES = os.environ.get('LAKEFS_DEFAULT_POLICIES', 'AuthManageOwnCredentials,FSReadAll')
 DRY_RUN = os.environ.get('DRY_RUN', 'false').lower() == 'true'
 
@@ -33,12 +34,12 @@ class EntraID:
     def __init__(self, tenant_id, application_id, client_secret):
         self._access_token = EntraID._get_access_token(
             tenant_id, application_id, client_secret)
-    
+
     @staticmethod
     def _get_access_token(tenant_id, application_id, client_secret) -> str:
         scope = ['https://graph.microsoft.com/.default']
         app = msal.ConfidentialClientApplication(
-            application_id, 
+            application_id,
             authority=f'https://login.microsoftonline.com/{tenant_id}',
             client_credential=client_secret,
         )
@@ -46,7 +47,7 @@ class EntraID:
         if not result:
             result = app.acquire_token_for_client(scopes=scope)
         return result.get('access_token')
-    
+
     def _lookup(self, path: str, params: Optional[dict[str, str]] = None):
         auth_headers = {'Authorization': f'Bearer {self._access_token}'}
         uri = requote_uri('https://graph.microsoft.com/v1.0' + path)
@@ -63,6 +64,10 @@ class EntraID:
         groups = self._lookup('/groups')
         return [group.get(key) for group in groups]
 
+    def get_user_names(self, key='mail') -> List[str]:
+        names = self._lookup('/users')
+        return [name.get(key) for name in names if name.get("officeLocation") == "Basel Office"]
+
 
 class LakeFSAuth:
 
@@ -73,7 +78,7 @@ class LakeFSAuth:
             password=secret_access_key,
         )
         self.client = LakeFSClient(configuration)
-    
+
     def _pagination_helper(page_fetcher, **kwargs):
         while True:
             resp = page_fetcher(**kwargs)
@@ -81,11 +86,11 @@ class LakeFSAuth:
             if not resp.pagination.has_more:
                 break
             kwargs['after'] = resp.pagination.next_offset
-    
+
     def get_group_names(self) -> Iterator[str]:
         for group in LakeFSAuth._pagination_helper(self.client.auth_api.list_groups):
             yield group.id
-    
+
     def create_group(self, group_id: str, exist_ok: bool = True):
         try:
             self.client.auth_api.create_group(GroupCreation(id=group_id))
@@ -93,14 +98,24 @@ class LakeFSAuth:
             if e.status == 409 and exist_ok:
                 return
             raise e
-        
+
     def attach_policy_to_group(self, policy_id, group_id):
         self.client.auth_api.attach_policy_to_group(
             group_id=group_id, policy_id=policy_id)
-    
 
 
-def sync_groups(entra: EntraID, lakefs: LakeFSAuth, group_filter: Optional[str] = None, default_policies: Optional[List[str]] = None, dry_run=True):
+def sync_users(entra: EntraID, lakefs: LakeFSAuth, user_filter: Optional[str] = None,
+               default_policies: Optional[List[str]] = None, dry_run=True):
+    filtered_users = entra.get_user_names()
+    if user_filter:
+        filtered_users = [item for item in filtered_users if not fnmatch.fnmatch(item, user_filter)]
+    print(f'found {len(filtered_users)} matching users')
+    for user in filtered_users:
+        print(user)
+
+
+def sync_groups(entra: EntraID, lakefs: LakeFSAuth, group_filter: Optional[str] = None,
+                default_policies: Optional[List[str]] = None, dry_run=True):
     # Get groups from EntraID
     filtered_groups = entra.get_group_names()
     if group_filter:
@@ -129,16 +144,16 @@ def sync_groups(entra: EntraID, lakefs: LakeFSAuth, group_filter: Optional[str] 
                         print(f'attach policy "{policy_id}" to group "{group_id}"')
                     else:
                         lakefs.attach_policy_to_group(policy_id, group_id)
-        
+
         # Done!
         print(f'Done syncing group: "{group_id}"')
 
 
-
 if __name__ == '__main__':
-    entra = EntraID(ENTRA_TENANT_ID,ENTRA_APPLICATION_ID,ENTRA_CLIENT_SECRET)
+    entra = EntraID(ENTRA_TENANT_ID, ENTRA_APPLICATION_ID, ENTRA_CLIENT_SECRET)
     lakefs = LakeFSAuth(LAKEFS_ACCESS_KEY_ID, LAKEFS_SECRET_ACCESS_KEY, LAKEFS_ENDPOINT)
     policies = None
     if LAKEFS_DEFAULT_POLICIES:
         policies = [p.strip() for p in LAKEFS_DEFAULT_POLICIES.split(',')]
-    sync_groups(entra, lakefs, GROUP_MATCH, default_policies=policies, dry_run=DRY_RUN)
+    # sync_groups(entra, lakefs, GROUP_MATCH, default_policies=policies, dry_run=DRY_RUN)
+    sync_users(entra, lakefs, USER_MATCH, default_policies=policies, dry_run=DRY_RUN)
